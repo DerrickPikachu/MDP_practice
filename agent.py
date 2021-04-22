d9d62817d874e5d35a986be2c9864253580547c7
PikaChin <derrfgh4563@gmail.com>
2021-04-23 01:15:11 +0800

    Handle the floating point accuracy miss problem

--- agent.py
+++ agent.py
@@ -1,6 +1,8 @@
 # My agent use MDP to learn to play the game define in game.py
 # I will use Value Iteration to learn the best policy,
 # reward is returned when the game is finished.
+import math
+
 import matplotlib.pyplot as plt
 
 
@@ -11,13 +13,16 @@ class MDP:
         self.value = [.0] * (self.coinLimit + 1)
         self.policy = [0] * (self.coinLimit + 1)
         self.value[100] = 1
-        self.terminateCondition = 1e-30
+        self.terminateCondition = 0
 
     def __evaluate(self, state, stacked):
         # (1-prob)*stateLose + prob*stateWin
         if state + stacked > self.coinLimit:
             return 0
         return (1 - self.headProb) * self.value[state - stacked] + self.headProb * self.value[state + stacked]
+
+    def __valueRound(self, value: float):
+        return math.floor(value * 100000) / 100000
 
     def valueIteration(self):
         delta = -1
@@ -32,16 +37,17 @@ class MDP:
             for i in range(1, self.coinLimit):
                 state = i
                 bestValue = 0
-                for j in range(1, min(state + 1, self.coinLimit - state + 1)):
+                for j in range(0, min(state + 1, self.coinLimit - state + 1)):
                     evaluateVal = self.__evaluate(state, j)
                     bestValue = max(bestValue, evaluateVal)
                 delta = max(delta, abs(self.value[i] - bestValue))
                 self.value[i] = bestValue
-            ax.plot(xlabel, self.value, label=str(iteration))
+            # ax.plot(xlabel, self.value, label=str(iteration))
             iteration += 1
             print('delta = {}'.format(delta))
             print('------------------')
 
+        ax.plot(xlabel, self.value, label=str(iteration))
         ax.legend()
         plt.show()
 
@@ -49,15 +55,25 @@ class MDP:
         for i in range(1, self.coinLimit):
             state = i
             bestValue = 0
+
+            # You 'Must' round the evaluated value!!
+            # That's because floating point isn't absolutely accuracy,
+            # and in this problem, there are always two choice which are the best and have equal value
+            # However, when the floating point miss appear, they sometime will have two different value with very
+            # low distinction.
+            # This give us the graph that is not very smooth and seems wrong (actually that graph is ok)
+            print('state: {}'.format(i))
             for j in range(1, min(state + 1, self.coinLimit - state + 1)):
                 evaluateVal = self.__evaluate(state, j)
+                evaluateVal = self.__valueRound(evaluateVal)
+                print('action: {}, state value: {}'.format(j, evaluateVal))
                 if bestValue < evaluateVal:
                     bestValue = evaluateVal
                     self.policy[i] = j
+            print('------------------')
 
         _, ax = plt.subplots()
         ax.plot(xlabel, self.policy)
-        ax.legend()
         plt.show()
 
     def getAction(self, state):
